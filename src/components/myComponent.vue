c3b53c9d7a480260d2627d2b024b4350c531610e
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-18 00:43:13 +0530

    Initial Commit

---
+++ src/components/myComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="">
+    <ul>
+      <li
+        class="number"
+        @click="handleClick"
+        :class="{ active: activeNum == 'One' }"
+      >
+        One
+      </li>
+      <li class="number">Two</li>
+      <li class="number">Three</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const activeNum = ref(null);
+
+const handleClick = (event) => {
+  if (activeNum.value == event.target.innerText) {
+    activeNum.value = null;
+  } else {
+    activeNum.value = event.target.innerText;
+  }
+};
+</script>
+
+<style scoped>
+.number {
+  cursor: pointer;
+  user-select: none;
+}
+.active {
+  color: blue;
+}
+</style>
+
---
+++ src/views/PlayView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an Play page</h1>
+    <myComponent />
+  </div>
+</template>
+
+<script setup>
+import myComponent from "@/components/myComponent.vue";
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
